1cd3970295a9c2c3bac6253e66b0d1b8028a07be
noooooorrrrf <167782532+noooooorrrrf@users.noreply.github.com>
2024-05-01 18:20:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/2e7e7b9b463149c
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/56d1760224589ee370186.jpg",
+    "https://github.com/noooooorrrrf",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
